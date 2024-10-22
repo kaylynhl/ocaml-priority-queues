7e3ccc09544a620b45ee93872ecbc7eb814230e7
Kaylyn Lee <khl62@cornell.edu>
2024-10-21 23:30:02 -0400

    Implement functor MakeListPQ
    - Wrote functor body in implementation
    - Added unit test cases and helpers
    - Added Str module to bin/dune
    - Impended

---
+++ lib/priorityQueues.ml
@@ -0,0 +1,80 @@
+(** A priority queue is a data abstraction that stores elements with associated
+    priorities. Elements are removed in order of priority. If elements are tied
+    in priority, then they are removed in order of insertion: an element that
+    was enqueued earlier is removed before an element that was enqueued later. *)
+module type PriorityQueue = sig
+  type elt
+  (** The type of an element in the priority queue. *)
+
+  type t
+  (** The type of a priority queue. *)
+
+  exception Empty
+  (** Raised when an operation cannot be performed because a priority queue is
+      empty. *)
+
+  val empty : t
+  (** [empty] is the empty priority queue. *)
+
+  val is_empty : t -> bool
+  (** [is_empty q] is [true] when [q] is the empty priority queue, and [false]
+      otherwise. *)
+
+  val enqueue : elt -> t -> t
+  (** [enqueue x q] is the priority queue that results from inserting [x] into
+      [q]. *)
+
+  val front : t -> elt
+  (** [front q] is the highest-priority element in [q]. Raises: [Empty] when [q]
+      is empty. *)
+
+  val dequeue : t -> t
+  (** [dequeue q] is the priority queue that results from deleting the
+      highest-priority element from [q]. Raises: [Empty] when [q] is empty. *)
+
+  val to_list : t -> elt list
+  (** [to_list q] is the list of elements in [q], in the order that they would
+      be removed from the queue. That is, the elements are listed in the order
+      they would be seen if we were to repeatedly call [front] and [dequeue]
+      until [q] ran empty. *)
+end
+
+module type Prioritizable = sig
+  type t
+
+  val priority : t -> int
+  (** [priority x] is a non-negative number representing the priority of [x].
+      Smaller integers represent higher priority. The highest priority is
+      therefore [0]. *)
+end
+
+module MakeListPQ (T : Prioritizable) : PriorityQueue with type elt = T.t =
+struct
+  type elt = T.t
+
+  (* AF: The list [a1; ...; an] represents a priority queue where the element
+     with the highest priority is at the front of the list. [] represents an
+     empty priority queue. *)
+  (* RI: The priority queue is sorted from highest priority to lowest priority.
+     That is, if element x has priority 0 it will be at the head of the queue.
+     Elements with the same priority are ordered such that the element inserted
+     first is earlier in the queue. *)
+  type t = elt list
+
+  exception Empty
+
+  let empty = []
+  let is_empty q = q = empty
+
+  let rec enqueue x q =
+    match q with
+    | [] -> [ x ]
+    | h :: t ->
+        if T.priority h = T.priority x then h :: x :: t
+        else if T.priority h > T.priority x then x :: h :: t
+        else h :: enqueue x t
+
+  let to_list q = q
+  let front q = if is_empty q then raise Empty else List.hd q
+  let dequeue q = if is_empty q then raise Empty else List.tl q
+end
--- test/test_a4.ml
+++ test/test_a4.ml
@@ -1,6 +1,99 @@
 open OUnit2
+open A4.PriorityQueues
 
+(* Test case modules *)
+module LengthPrioritizedString = struct
+  include String
+
+  (** The priority of a string is its length. Example: [priority "hello" = 5] *)
+  let priority s = length s
+end
+
+module StringPQ = MakeListPQ (LengthPrioritizedString)
+
+(* Test case helpers *)
+
+let test_is_empty_true =
+  "Testing is_empty" >:: fun _ ->
+  let q = StringPQ.empty in
+  assert_equal true (StringPQ.is_empty q) ~printer:string_of_bool
+
+let test_enqueue_blackbox =
+  "Blackbox testing enqueue" >:: fun _ ->
+  let q = StringPQ.enqueue "test" StringPQ.empty in
+  assert_equal false (StringPQ.is_empty q) ~printer:string_of_bool
+
+let test_enqueue_same_priorities =
+  "Testing enqueue" >:: fun _ ->
+  let q = StringPQ.enqueue "test" StringPQ.empty in
+  let q = StringPQ.enqueue "work" q in
+  let q = StringPQ.enqueue "satoru" q in
+  let q = StringPQ.enqueue "reigen" q in
+  assert_equal
+    [ "test"; "work"; "satoru"; "reigen" ]
+    (StringPQ.to_list q) ~printer:(String.concat "; ")
+
+let test_enqueue =
+  "Testing enqueue" >:: fun _ ->
+  let q = StringPQ.enqueue "test" StringPQ.empty in
+  assert_equal [ "test" ] (StringPQ.to_list q) ~printer:(String.concat "; ")
+
+let test_front_empty =
+  "Testing front with empty PQ raises error" >:: fun _ ->
+  let q = StringPQ.empty in
+  assert_raises StringPQ.Empty (fun _ -> StringPQ.front q)
+
+let test_front =
+  "Testing front" >:: fun _ ->
+  let q = StringPQ.enqueue "test" StringPQ.empty in
+  let q = StringPQ.enqueue "works" q in
+  let q = StringPQ.enqueue "work" q in
+  assert_equal "test" (StringPQ.front q) ~printer:Fun.id
+
+let test_dequeue_blackbox =
+  "Blackbox testing dequeue" >:: fun _ ->
+  let q = StringPQ.enqueue "test" StringPQ.empty in
+  let q = StringPQ.dequeue q in
+  assert_equal true (StringPQ.is_empty q) ~printer:string_of_bool
+
+let test_dequeue_empty =
+  "Testing dequeue with empty PQ raises error" >:: fun _ ->
+  let q = StringPQ.empty in
+  assert_raises StringPQ.Empty (fun _ -> StringPQ.dequeue q)
+
+let test_dequeue =
+  "Testing dequeue" >:: fun _ ->
+  let q = StringPQ.enqueue "test" StringPQ.empty in
+  let q = StringPQ.enqueue "test2" q in
+  let q = StringPQ.dequeue q in
+  assert_equal [ "test2" ] (StringPQ.to_list q) ~printer:(String.concat "; ")
+
+let test_string_pq =
+  "Testing sample test case StringPQ" >:: fun _ ->
+  let q = StringPQ.enqueue "hello" StringPQ.empty in
+  let q = StringPQ.enqueue "world" q in
+  let q = StringPQ.enqueue "this" q in
+  let q = StringPQ.enqueue "is" q in
+  let q = StringPQ.enqueue "a" q in
+  let q = StringPQ.enqueue "test" q in
+  assert_equal
+    [ "a"; "is"; "this"; "test"; "hello"; "world" ]
+    (StringPQ.to_list q) ~printer:(String.concat "; ")
+
+(* Test suite *)
 let tests =
-  "test suite" >::: [ ("a trivial test" >:: fun _ -> assert_equal 0 0) ]
+  "test suite"
+  >::: [
+         test_is_empty_true;
+         test_enqueue_blackbox;
+         test_enqueue_same_priorities;
+         test_enqueue;
+         test_front_empty;
+         test_front;
+         test_dequeue_blackbox;
+         test_dequeue_empty;
+         test_dequeue;
+         test_string_pq;
+       ]
 
 let _ = run_test_tt_main tests
